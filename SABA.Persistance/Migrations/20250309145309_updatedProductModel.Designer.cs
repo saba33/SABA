103d424c4379776261a3f4a710c58ecf68e7f62e
saba33 <49198814+saba33@users.noreply.github.com>
2025-03-09 19:01:16 +0400

    fixed connection sdtring

--- SABA.Persistance/Migrations/20250309145309_updatedProductModel.Designer.cs
+++ SABA.Persistance/Migrations/20250309145309_updatedProductModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SABA.Persistance.DataContext;
 
@@ -11,9 +12,11 @@ using SABA.Persistance.DataContext;
 namespace SABA.Persistance.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250309145309_updatedProductModel")]
+    partial class updatedProductModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SABA.Persistance/Migrations/20250309145309_updatedProductModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SABA.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductType",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductType",
+                table: "Products");
+        }
+    }
+}
