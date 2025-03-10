a7477861b7bdb02f147bce1c460fc4f6f9d90308
saba33 <49198814+saba33@users.noreply.github.com>
2025-03-11 02:11:59 +0400

    fixef connection string

--- SABA.web/Program.cs
+++ SABA.web/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDependencies(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
