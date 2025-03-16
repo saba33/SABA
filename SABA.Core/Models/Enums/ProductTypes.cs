4e7e19317b35869253f9573b232007732fe40b1c
saba33 <49198814+saba33@users.noreply.github.com>
2025-03-16 14:09:38 +0400

    Ferat: ProductTypes, changes

--- SABA.Core/Models/Enums/ProductTypes.cs
+++ SABA.Core/Models/Enums/ProductTypes.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SABA.Core.Models.Enums
+﻿namespace SABA.Core.Models.Enums
 {
     public enum ProductTypes
     {
         JacetsAndCoats = 1,
         Dresses = 2,
-        Bags
+        Bags = 3
     }
 }
---
+++ SABA.web/Controllers/GetProductTypes.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SABA.Services.Models.Enum;
+
+namespace SABA.web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetProductTypes : ControllerBase
+    {
+        [HttpGet("GetProductTypesForMainPage")]
+        public ActionResult getProductTypes()
+        {
+            var types = Enum.GetValues(typeof(ProductType))
+                            .Cast<ProductType>()
+                            .ToDictionary(t => t.ToString(), t => (int)t);
+
+            return Ok(types);
+        }
+    }
+}
